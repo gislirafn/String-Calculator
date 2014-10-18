8d63e36fdf667f72844636b7483d32b567d2a95b
gislirafn <gisli13@ru.is>
2014-10-18 18:35:35 +0000

    Step 7 complete. Added a function similar to the previous delimiter function, but adds a delimiter of any length to the string of delimiters. Also added a if-statement to the sum function, to check whether it's adding an empty character. Two test cases added to verify the results.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -8,22 +8,14 @@ public class Calculator {
 
 		if(text.equals(""))
 			return 0;
-        else if(text.startsWith("//"))
-            return sum(splitNumbersWithCustomDelimiter(text, delimiters));
         else if(text.contains("-"))
             return sum(splitNumbers(text, delimiters));
+        else if(text.contains("//["))
+            return sum(splitNumbersWithAnyLengthDelimiter(text, delimiters));
+        else if(text.startsWith("//"))
+            return sum(splitNumbersWithCustomDelimiter(text, delimiters));
         else
             return sum(splitNumbers(text, delimiters));
-
-
-/*        if(text.startsWith("//")){
-            delimiters += "|" + text.substring(2, 3);
-            text = text.substring(4);
-            return sum(splitNumbers(text, delimiters));
-        }
-        else
-            return sum(splitNumbers(text, delimiters));*/
-
     }
 
 	private static int toInt(String numbers){
@@ -40,35 +32,30 @@ public class Calculator {
         return numbers.split(delimiters);
     }
 
+    private static String[] splitNumbersWithAnyLengthDelimiter(String numbers, String delimiters){
+        int newLineIndex = numbers.indexOf("\n");
+        delimiters += "|" + numbers.substring(2, newLineIndex);
+        numbers = numbers.substring(newLineIndex + 1);
+        return numbers.split(delimiters);
+    }
+
     private static int sum(String[] numbers){
  	    int total = 0;
         String neg = "";
         for(String number : numbers){
+            if(number.equals(""))
+                continue;
+            if(toInt(number) > 1000)
+                continue;
             if(toInt(number) < 0)
                 neg += number + ",";
 		    total += toInt(number);
 		}
         if(!neg.equals(""))
-            throw new RuntimeException("Negatives not allowed: " + neg.substring(0, neg.length() - 1));
+            throw new RuntimeException("Negatives not allowed: " + neg.substring(0, neg.length() -1));
 
 		return total;
     }
-
-   /* private static void checkNegatives(String[] numbers){
-        String neg = "";
-        for(String s : arr){
-            if(s.equals("-"))
-                neg += "," + s;
-        }
-
-        if(!"".equals(neg))
-            throw new Exception("Negatives not allowed: " + neg.substring(1));
-    }*/
-
-    /*private static void checkSize(String numbers){
-        for(String number : numbers){
-            if(toInt(number) > 1000).....           // Herna vantar liklega splittid
-    */
 
     public static void main(String[] args){
         System.out.print("hello");
